bb7bf4cf330683525e08f9f729b7fac27d725c3e
s17 <ousmanejrsylla@hotmail.com>
2023-05-22 21:28:12 +0000

    Authentication handled.

---
+++ src/http/auth-api.js
@@ -0,0 +1,9 @@
+import {api} from './api';
+
+export const csrfCookie = () => api.get('/sanctum/csrf-cookie');
+
+export const login = (credentials) => api.post('/auth/login', credentials);
+
+export const register = (user) => api.post('/auth/register', user);
+
+export const getUser = () => api.get('/api/user');
--- src/http/task-api.js
+++ src/http/task-api.js
@@ -1,7 +1,7 @@
 import api from './api';
 
 const apiPath = import.meta.env.VITE_API_PATH;
-const resource = apiPath + 'tasks';
+const resource = apiPath + '/tasks';
 
 export const allTasks = () => api.get(resource);
 
